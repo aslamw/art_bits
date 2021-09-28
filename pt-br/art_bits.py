84efda90d80655aac93ea5a50c3ff36e632ec94d
Monkey <newsmallmonkey@gmail.com>
2021-09-28 17:26:10 -0300

    start version 1.0

--- pt-br/art_bits.py
+++ pt-br/art_bits.py
@@ -12,11 +12,13 @@ class bit(object):#tratamento do desenho
         self.linha = linha
         self.coluna = coluna
         self.espaco = ''
+        self.esqueleto = ''
         self.key = {'manual':'print(self.__str__())',
-                    'lin':'self.lin()','matrix':'print(self.matrix)',
+                    'lin':'self.lin()','matriz':'print(self.matrix)',
                     'linha':'print(self.linha)','coluna':'print(self.coluna)',
-                    'espaço':'print(self.espaco)','uni':'self.uni()',
-                    'clear':'self.limpo()'
+                    'H':'print(self.espaco)','uni':'self.uni()',
+                    'clear':'self.limpo()','esqueleto':'print(self.esqueleto)',
+                    'visual':'print(self.espaco)','uni -r':'self.uni(0)'
                     }#comandos
         
     def comandos(self):
@@ -25,82 +27,107 @@ class bit(object):#tratamento do desenho
         
         while 1:
             escolha = input('_> ')#terminal 
+            
             if escolha in self.key: eval(f'{self.key[escolha]}')#tratamento de comandos
             
             elif escolha == 'x': break#fim do loop
             
-            elif escolha == 'visual': print(self.campo())
-            
             else:print('esse camando não é valido')
             
-    def uni(self):
+    def uni(self,n=1):
         try:
             while 1:
-                linha = int(input('L_> '))
-                coluna = int(input('C_> '))
-                self.campo()
+                linha = int(input('Linha_> '))
+                coluna = int(input('Coluna_> '))
                 
-                self.matrix[linha][coluna] = 0 
+                self.matrix[linha][coluna] = n
+                
+                self.campo()
         except:
             self.comandos()
             
-    def lin(self):
+    def lin(self,n=1):
         #define a linha do meio-----------------------
         while 1:
-            pontoN = input('digite a linha ou coluna limite -EX.: L 3--')
+            pontoN = input('digite a linha L ou coluna C __-EX.: L 3--> ')
             if pontoN == 'x':break
             
-            pontoL = int(input('digite -linha-'))
-            pontoC = int(input('digite -coluna-'))
+            pontoL = int(input('digite -linha-> '))
+            pontoC = int(input('digite -coluna-> '))
+            
             pontoN = pontoN.split()
             pontoN[1]=int(pontoN[1])
             
-            if pontoN[0] == 'L':
+            if pontoN[0] == 'L':#modelo por linha
                 if pontoN[1] > pontoL:
-                    for i in range(pontoL,pontoN[1]): self.matrix[i][pontoC] = 0
-                                   
+                    for i in range(pontoL,pontoN[1]): self.matrix[i][pontoC] = n
+                    
                 else:
-                    for i in range(pontoN[1],pontoL): self.matrix[i][pontoC] = 0        
+                    for i in range(pontoN[1],pontoL): self.matrix[i][pontoC] = n
             
-            elif pontoN[0] == 'C':
+            elif pontoN[0] == 'C':#modelo por coluna
                 if pontoN[1] > pontoC:
-                    for i in range(pontoC,pontoN[1]): self.matrix[pontoL][i] = 0
+                    for i in range(pontoC,pontoN[1]): self.matrix[pontoL][i] = n
                                    
                 else:
-                    for i in range(pontoN[1],pontoC): self.matrix[pontoL][i] = 0
+                    for i in range(pontoN[1],pontoC): self.matrix[pontoL][i] = n
+            self.campo()
 
     def campo(self):
         #cria o espaço no campo-------------------------
         self.espaco = ''#limpesa de campo
+        self.esqueleto = ''
         for i in range(self.linha):
             
             self.espaco += '\n'
+            self.esqueleto += '\n' + f'{i} : '
             for o in range(self.coluna):
                 if self.matrix[i][o] == 0:self.espaco += '  '        
-            
+                
                 else: self.espaco += ' @'
-        print(self.espaco)#apresentação
+                self.esqueleto += str(o) + ' |'
 
     def __str__(self):#manual---------------------------------
-        return 'foobarr é bom'
+        return f'''
+                                ---------Comandos-------------
+                                
+                  x    __________sair
+                  uni  _____________desenho por unidade
+                  lin  _____________desenho por linha ou coluna
+                  visual____________visualizar imagem
+                  matriz____________apresenta a matriz
+                  linha_____________mostra a quantidade de linhas na matriz
+                  coluna____________mostra a quantidade de coluna na matriz
+                  H    _____________mostra o desenho sem 
+                  -r   ____________para remover, use <*uni -r *> ou <* lin -r*>
+    
+                  
+                  
+                               ---------modelo da matriz----------
+                               
+                  {self.esqueleto}
+                  
+                  Oque está a esquerda de * : * é contado como linha e
+                  tudo a direita é coluna.
+            ''' 
     def limpo(self):
-        return os.system('cls')
-        
-        
+        return os.system('cls')          
+      
 while True:
     #tamanho do campo
-    matrizL = int(input('digite linhas --limite 50--: '))
-    if matrizL == 00:break#-----fim-------------------
+    try:
+        matrizL = int(input('digite linhas da matriz --limite 50--: '))    
+        matrizC = int(input('digite colunas da matriz --limite 20--: '))
     
-    matrizC = int(input('digite colunas --limite 20--: '))
-    
-    #ajuste de limite
-    if matrizL > 50:matrizL = 50
-    if matrizC > 20: matrizC = 20
-    
-    matrix = numpy.ones(shape=(matrizL,matrizC))#cria a matrix
-    matrix = matrix.astype(int)
-    
-    art = bit(matrix,matrizL,matrizC)
-    art.campo()
-    art.comandos()
+        #ajuste de limite
+        if matrizL > 50:matrizL = 50
+        if matrizC > 20: matrizC = 20
+        
+        matrix = numpy.zeros(shape=(matrizL,matrizC))#cria a matrix
+        matrix = matrix.astype(int)
+        
+        art = bit(matrix,matrizL,matrizC)
+        art.campo()
+        art.comandos()
+    except:
+        break
