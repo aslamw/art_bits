98f3efb3d3bc30a26224ff52654c5cbab95aba52
Marcos <wgngui14@gmail.com>
2023-08-11 15:57:54 -0300

    adjust for module

---
+++ pt-br/test.py
@@ -0,0 +1,14 @@
+from art_bits import ArtBit as bt
+
+art = bt(50,50)
+
+
+art.lin("L 5",1,6)
+art.lin("C 11",1,6)
+art.lin("L 5",1,11)
+art.lin("C 11",5,6)
+
+
+#art.open_date("teste.dll")
+
+art.campo()
